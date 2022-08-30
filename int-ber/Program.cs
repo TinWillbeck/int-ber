af2446551839701a8937a2590d4e471595a85d05
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2022-08-30 10:31:49 +0200

    kjygjkhyg

---
+++ int-ber/Program.cs
@@ -0,0 +1,22 @@
+﻿string input;
+input = ("");
+
+while  (input != "äpple" && input != "päron")
+{
+    Console.WriteLine("äpple eller päron?");
+    input = Console.ReadLine();
+}
+    if (input == "äpple")
+    {
+        Console.WriteLine("bra!");
+    }
+    else if (input == "päron")
+    {
+        Console.WriteLine("dåligt!");
+    }
+    else if (input != "äpple" && input != "päron")
+    {
+        Console.WriteLine("dåligt svar");
+    }
+
+Console.ReadLine();
